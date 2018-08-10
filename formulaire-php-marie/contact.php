ee2f3ae1876090611fb326d91da141a438900316
MCambie <cambie.marie@gmail.com>
2018-08-10 15:17:14 +0200

    remise php normal

--- formulaire-php-marie/contact.php
+++ formulaire-php-marie/contact.php
@@ -2,9 +2,24 @@
 session_start();
 include('Partials/header.php'); ?>
 
+<?php if (array_key_exists('errors', $_SESSION)): ?>
+
+<div class="alert alert-danger">
+<?= implode('<br>', $_SESSION['errors']); ?>
+</div>
+
+<?php  endif;  ?>
+
+<?php if (array_key_exists('success', $_SESSION)): ?>
+
+<div class="alert alert-success">
+Votre mail à bien été envoyé
+</div>
+
+<?php  endif;  ?>
 
 
-<form action="https://formspree.io/cambie.marie@gmail.com"
+<form action="post_contact.php"
       method="POST">
       <div class="form-group">
         <div class="row container">
